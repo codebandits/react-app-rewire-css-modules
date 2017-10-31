9cb7f49c6db86025ed1ef23f50514c26bf8bbdfb
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-10-31 03:07:18 -0500

    Cleanup function and variable names

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
+const cloneDeep = require('lodash.clonedeep')
 
 const ruleChildren = (loader) => loader.use || loader.oneOf || Array.isArray(loader.loader) && loader.loader || []
 
-const findRulesWithMatchingRule = (rulesSource, matcher) => {
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
     let result = undefined
     const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource)
-    rules.some((rule, index) => result = matcher(rule) ? {index, rules} : findRulesWithMatchingRule(ruleChildren(rule), matcher))
+    rules.some((rule, index) => result = ruleMatcher(rule) ? {index, rules} : findIndexAndRules(ruleChildren(rule), ruleMatcher))
     return result
 }
 
-const getRule = (rulesSource, matcher) => {
-    const {index, rules} = findRulesWithMatchingRule(rulesSource, matcher)
+const findRule = (rulesSource, ruleMatcher) => {
+    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher)
     return rules[index]
 }
+
+const cssRuleMatcher = (rule) => rule.test && String(rule.test) === String(/\.css$/)
 
 const createLoaderMatcher = (loader) => (rule) => rule.loader && rule.loader.indexOf(`/${loader}/`) !== -1
 const cssLoaderMatcher = createLoaderMatcher('css-loader')
 const postcssLoaderMatcher = createLoaderMatcher('postcss-loader')
 const fileLoaderMatcher = createLoaderMatcher('file-loader')
-const cssRuleMatcher = (rule) => rule.test && String(rule.test) === String(/\.css$/)
 
-const cloneDeep = require('lodash.clonedeep')
-
-const addAfterLoader = (rulesSource, matcher, value) => {
-    const {index, rules} = findRulesWithMatchingRule(rulesSource, matcher)
+const addAfterRule = (rulesSource, ruleMatcher, value) => {
+    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher)
     rules.splice(index + 1, 0, value)
 }
 
-const addBeforeLoader = (rulesSource, matcher, value) => {
-    const {index, rules} = findRulesWithMatchingRule(rulesSource, matcher)
+const addBeforeRule = (rulesSource, ruleMatcher, value) => {
+    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher)
     rules.splice(index, 0, value)
 }
 
 module.exports = function (config, env) {
-    const cssRule = getRule(config.module.rules, cssRuleMatcher)
-    const cssRuleCssLoader = getRule(cssRule, cssLoaderMatcher)
+    const cssRule = findRule(config.module.rules, cssRuleMatcher)
+    const cssRuleCssLoader = findRule(cssRule, cssLoaderMatcher)
 
     cssRuleCssLoader.options = Object.assign({modules: true, localIdentName: '[local]___[hash:base64:5]'}, cssRuleCssLoader.options)
 
     const sassRule = cloneDeep(cssRule)
     sassRule.test = /\.s[ac]ss$/
-    addAfterLoader(sassRule, postcssLoaderMatcher, require.resolve('sass-loader'))
-
-    addBeforeLoader(config.module.rules, fileLoaderMatcher, sassRule)
+    addAfterRule(sassRule, postcssLoaderMatcher, require.resolve('sass-loader'))
+    addBeforeRule(config.module.rules, fileLoaderMatcher, sassRule)
 
     return config
 }
